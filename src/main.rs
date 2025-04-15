3341a4041db313a47e3dc9895ca5e0cab09595ac
elmer-ns <elmer.ns@outlook.com>
2025-04-15 18:07:30 +0200

    Include icon in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{error::Error, path::Path};
+use std::error::Error;
+use image;
+use slint::{Rgba8Pixel, SharedPixelBuffer};
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    ui.set_ico(slint::Image::load_from_path(Path::new("./icon.ico")).unwrap());
+    let icon = load_icon()?;
+    ui.set_ico(slint::Image::from_rgba8(icon));
 
     ui.run()?;
     Ok(())
+}
+
+fn load_icon() -> Result<SharedPixelBuffer<Rgba8Pixel>, Box<dyn Error>> {
+    let icon_raw = include_bytes!("../icon.ico");
+    let icon = image::load_from_memory(icon_raw)?
+        .to_rgba8();
+
+    Ok(SharedPixelBuffer::clone_from_slice(icon.as_raw(), icon.width(), icon.height()))
 }
