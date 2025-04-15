214ee761e104b3345d8c8a25f099d28bf6d8f664
elmer-ns <elmer.ns@outlook.com>
2025-04-15 17:28:20 +0200

    Add icon and clean up UI

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::error::Error;
+use std::{error::Error, path::Path};
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    println!("RUST");
-
-    ui.on_verb_edited({
-        let ui_handle = ui.as_weak();
-        move |verb| {
-            let ui = ui_handle.unwrap();
-            ui.set_verb(verb);
-        }
-    });
+    ui.set_ico(slint::Image::load_from_path(Path::new("./icon.ico")).unwrap());
 
     ui.run()?;
     Ok(())
