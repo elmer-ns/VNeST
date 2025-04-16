ea6c6c23a4bec77bbabc363bc0cd4478124666d6
elmer-ns <elmer.ns@outlook.com>
2025-04-16 10:17:46 +0200

    Font size and text limit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use std::error::Error;
 use image;
 use slint::{Rgba8Pixel, SharedPixelBuffer};
+use text_measurer_harfbuzz::measure_text_width;
 
 slint::include_modules!();
 
@@ -12,6 +13,21 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let icon = load_icon()?;
     ui.set_ico(slint::Image::from_rgba8(icon));
+
+    ui.global::<ValidateString>().on_limit_size({
+        let ui_handle = ui.as_weak();
+        move |mut string: slint::SharedString, max_length: f32,font_size: f32| {
+            let ui = ui_handle.unwrap();
+            
+            if string == "" {return string}
+
+            while measure_text_width(include_bytes!("../SpaceMono-Regular.ttf").to_vec(), font_size as u32, &string).unwrap() as f32 > max_length*0.85 {
+                string = string.split_at(string.len()-1).0.into()
+            }
+   
+            string
+        }
+    });
 
     ui.run()?;
     Ok(())
