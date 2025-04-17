572b2fe26fda165ab358c424063bca2ebc4b586b
elmer-ns <elmer.ns@outlook.com>
2025-04-17 13:55:15 +0200

    Refactor and fix font-size

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::error::Error;
+use std::{error::Error, string};
 use image;
 use slint::{Rgba8Pixel, SharedPixelBuffer};
 use text_measurer_harfbuzz::measure_text_width;
@@ -17,15 +17,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ui.global::<ValidateString>().on_limit_size({
         //let ui_handle = ui.as_weak();
         move |mut string: slint::SharedString, max_length: f32,font_size: f32| {
-            //let ui = ui_handle.unwrap();
-            
-            if string == "" {return string}
-            
-            while measure_text_width(include_bytes!("../SpaceMono-Regular.ttf").to_vec(), font_size as u32, &string).unwrap() as f32 > max_length*0.85 {
-                string = string.split_at(string.len()-1).0.into()
-            }
-            
-            string
+
+            if string == "" {return font_size}
+
+            font_size_calc(font_size, string.into(), max_length)
         }
     });
 
@@ -39,4 +34,20 @@ fn load_icon() -> Result<SharedPixelBuffer<Rgba8Pixel>, Box<dyn Error>> {
         .to_rgba8();
 
     Ok(SharedPixelBuffer::clone_from_slice(icon.as_raw(), icon.width(), icon.height()))
+}
+
+fn font_size_calc(start_size: f32, string: String, max_width: f32) -> f32 {
+    let mut step_size: f32 = 0.5;
+
+    let mut size = start_size;
+
+    while measure_text_width(include_bytes!("../SpaceMono-Regular.ttf").to_vec(), size as u32, &string).unwrap() as f32 > max_width*0.9 {
+        size -= step_size;
+        if size < 0.0 {
+            size += step_size;
+            step_size *= 0.5;
+        }
+    }
+
+    size
 }
