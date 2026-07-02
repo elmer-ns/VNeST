0583ad23c002bfc63adaee0f75b68fc16b1c6be0
Elmer NS <174507028+elmer-ns@users.noreply.github.com>
2026-07-02 14:29:45 +0200

    WASM

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 slint::include_modules!();
 
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = AppWindow::new()?;
 
